5059146af62de2b391daeff0d3c6ba82e6fac72f
Sona Varghese <sonasusan.v@gmail.com>
2020-11-10 02:08:47 +0000

    Commit server side rendering changes

--- express_server.js
+++ express_server.js
@@ -16,6 +16,11 @@ app.get('/urls.json', (req, res) => {
   res.send(urlDatabase);
 });
 
+app.get('/urls', (req, res) => {
+  const templateVar = {url: urlDatabase};
+  res.render('urls_index', templateVar);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
